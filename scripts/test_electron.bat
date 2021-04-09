4fad8f1b9eff651699509de469c18fa6b5d40faa
Benjamin Pasero <benjamin.pasero@microsoft.com>
2021-04-09 11:34:39 +0200

    fix tests

--- scripts/test_electron.bat
+++ scripts/test_electron.bat
@@ -1,5 +1,13 @@
 @echo off
 setlocal
 
+pushd %~dp0\..
+
 set ELECTRON_RUN_AS_NODE=1 
-.\node_modules\.bin\electron .\node_modules\.bin\mocha -R spec --timeout 480000
+
+call .\node_modules\.bin\electron .\test\support/createdb.js
+call .\node_modules\.bin\electron .\node_modules\.bin\mocha -R spec --timeout 480000
+
+popd
+
+endlocal
--- scripts/test_electron.sh
+++ scripts/test_electron.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-ELECTRON_RUN_AS_NODE=1 ./node_modules/.bin/electron ./node_modules/.bin/mocha -R spec --timeout 480000
+export ELECTRON_RUN_AS_NODE=1 
+
+./node_modules/.bin/electron ./test/support/createdb.js
+./node_modules/.bin/electron ./node_modules/.bin/mocha -R spec --timeout 480000
